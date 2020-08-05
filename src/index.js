bdf64f6bf708a86ea7e650435cdf58540b461750
Keshav singh <61233273+keshavsingh123@users.noreply.github.com>
2020-08-05 14:52:09 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//const citrus=["lime" ,"lemon"];
+//const fruits=["orange","banana" ,...citrus];
+//console.log(fruits);
+//spread operator {...citrus}
